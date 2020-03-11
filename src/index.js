d815e5f536f4bfd7337624224838e83b5025a7b0
Hanna Lee <mani21na@gmail.com>
2020-03-11 03:47:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -59,9 +59,10 @@ function textColor() {
 }
 **/
 
+
 let breeds = [];
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   loadImages();
   loadBreedOptions();
 });
@@ -69,14 +70,14 @@ document.addEventListener('DOMContentLoaded', function() {
 function loadImages() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
   fetch(imgUrl)
-    .then(resp => resp.json())
+    .then(res=> res.json())
     .then(results => {
       results.message.forEach(image => addImage(image))
     });
 }
 
 function addImage(dogPicUrl) {
-  let container = document.queryElector('#dog-image-container');
+  let container = document.querySelector('#dog-image-container');
   let newImageEl = document.createElement('img');
   newImageEl.src = dogPicUrl;
   container.appendChild(newImageEl);
@@ -85,18 +86,38 @@ function addImage(dogPicUrl) {
 function loadBreedOptions() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
   fetch(breedUrl)
-  .then(res => res.json())
-  .then(results => {
-    breeds = Object.keys(results.message);
-    updateBreedList(breeds);
-    addBreedSelectListener();
-  });
+    .then(res => res.json())
+    .then(results => {
+
+      breeds = Object.keys(results.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
 }
 
 function updateBreedList(breeds) {
   let ul = document.querySelector('#dog-breeds');
   removeChildren(ul);
   breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(element) {
+  let child = element.lastElementChild;
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
+
+function selectBreedsStartingWith(letter) {
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+  breedDropdown.addEventListener('change', function (event) {
+    selectBreedsStartingWith(event.target.value);
+  });
 }
 
 function addBreed(breed) {
